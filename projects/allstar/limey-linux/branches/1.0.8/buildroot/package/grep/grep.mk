6151bdd1a50033b4db0965c61c89cc8cdb4389b7
steve <steve@b9360e03-a0fa-4602-804e-483810d782e8>
2009-10-07 05:41:31 +0000

    Revert back to manual method to make grep so that we can choose where to install it.

--- projects/allstar/limey-linux/branches/1.0.8/buildroot/package/grep/grep.mk
+++ projects/allstar/limey-linux/branches/1.0.8/buildroot/package/grep/grep.mk
@@ -3,12 +3,77 @@
 # grep
 #
 #############################################################
-GREP_VERSION:=2.5.3
-GREP_SOURCE:=grep-$(GREP_VERSION).tar.bz2
-GREP_SITE:=$(BR2_GNU_MIRROR)/grep
-GREP_CONF_OPT = --disable-perl-regexp --without-included-regex \
-	$(DISABLE_NLS) $(DISABLE_LARGEFILE)
+GNUGREP_VERSION:=2.5.3
+GNUGREP_SOURCE:=grep-$(GNUGREP_VERSION).tar.gz
+GNUGREP_SITE:=http://mirrors.kernel.org/gnu/grep
 
-GREP_DEPENDENCIES = $(if $(BR2_ENABLE_LOCALE),gettext libintl)
+GNUGREP_DIR:=$(BUILD_DIR)/grep-$(GNUGREP_VERSION)
+GNUGREP_CAT:=$(ZCAT)
+GNUGREP_BINARY:=src/grep
+GNUGREP_TARGET_BINARY:=bin/grep
 
-$(eval $(call AUTOTARGETS,package,grep))
+$(DL_DIR)/$(GNUGREP_SOURCE):
+	$(call DOWNLOAD,$(GNUGREP_SITE),$(GNUGREP_SOURCE))
+
+$(GNUGREP_DIR)/.source: $(DL_DIR)/$(GNUGREP_SOURCE)
+	$(ZCAT) $(DL_DIR)/$(GNUGREP_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
+	touch $@
+
+$(GNUGREP_DIR)/.configured: $(GNUGREP_DIR)/.source
+	(cd $(GNUGREP_DIR); rm -rf config.cache; \
+		$(TARGET_CONFIGURE_OPTS) \
+		CFLAGS="$(TARGET_CFLAGS)" \
+		./configure \
+		--target=$(GNU_TARGET_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--build=$(GNU_HOST_NAME) \
+		--prefix=/usr \
+		--exec-prefix=/usr \
+		--bindir=/usr/bin \
+		--sbindir=/usr/sbin \
+		--libdir=/lib \
+		--libexecdir=/usr/lib \
+		--sysconfdir=/etc \
+		--datadir=/usr/share \
+		--localstatedir=/var \
+		--mandir=/usr/man \
+		--infodir=/usr/info \
+		$(DISABLE_NLS) \
+		$(DISABLE_LARGEFILE) \
+		--disable-perl-regexp \
+		--without-included-regex \
+	);
+	touch $@
+
+$(GNUGREP_DIR)/$(GNUGREP_BINARY): $(GNUGREP_DIR)/.configured
+	$(MAKE) -C $(GNUGREP_DIR)
+
+# This stuff is needed to work around GNU make deficiencies
+grep-target_binary: $(GNUGREP_DIR)/$(GNUGREP_BINARY)
+	@if [ -L $(TARGET_DIR)/$(GNUGREP_TARGET_BINARY) ] ; then \
+		rm -f $(TARGET_DIR)/$(GNUGREP_TARGET_BINARY); fi;
+	set -x; \
+	rm -f $(TARGET_DIR)/bin/grep $(TARGET_DIR)/bin/egrep $(TARGET_DIR)/bin/fgrep; \
+	cp -a $(GNUGREP_DIR)/src/grep $(GNUGREP_DIR)/src/egrep \
+	$(GNUGREP_DIR)/src/fgrep $(TARGET_DIR)/bin/;
+
+grep: uclibc grep-target_binary
+
+grep-source:	$(DL_DIR)/$(GNUGREP_SOURCE)
+
+grep-clean:
+	$(MAKE) DESTDIR=$(TARGET_DIR) -C $(GNUGREP_DIR) uninstall
+	-$(MAKE) -C $(GNUGREP_DIR) clean
+
+grep-dirclean:
+	rm -rf $(GNUGREP_DIR)
+
+#############################################################
+#
+# Toplevel Makefile options
+#
+#############################################################
+ifeq ($(strip $(BR2_PACKAGE_GREP)),y)
+TARGETS+=grep
+endif
+
